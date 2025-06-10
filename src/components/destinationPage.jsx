ff4dfc9d1cefc4000321b2d440097e0a33cae560
unknownsteve7 <madicharlamohan7@gmail.com>
2025-06-10 19:52:30 +0530

    Initial commit

---
+++ src/components/destinationPage.jsx
@@ -0,0 +1,10 @@
+// src/components/DestinationPage.jsx
+import React from 'react';
+
+const DestinationPage = () => (
+  <div>
+    <h1>Welcome to the Destination Page!</h1>
+  </div>
+);
+
+export default DestinationPage;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontSize: {
+        '11xl': '11rem',
+      },
+    },
+  },
+  plugins: [],
+};
